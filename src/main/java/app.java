1a3c90adb63706acc8df6abe2093eb2f6985a4c8
aikokendo <alejandrameraz@gmail.com>
2020-01-07 23:38:18 -0500

    added insert statements

--- src/main/java/app.java
+++ src/main/java/app.java
@@ -1,43 +1,67 @@
 import java.io.*;
-import java.util.HashMap;
-import java.util.Map;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.stream.Collectors;
 
 import com.opencsv.CSVReader;
+import org.json.CDL;
 import org.json.JSONArray;
 import org.json.XML;
 
 public class app {
     public static void main(String[] args){
         JSONArray itemData = new JSONArray();
+
         if (args.length > 1) {   //parameters provided
             String csvFileName = args[0];
+            String csvJustName = csvFileName.split("\\.")[0];
             CSVReader reader = null;
-            try {
-                reader = new CSVReader(new FileReader(csvFileName));  //CSVReader manages default comma separated values behaviours.
-                String[] headers = reader.readNext(); //headers
-                String[] line;
-                while ((line = reader.readNext()) != null){
-                    Map<String, String> lineSelected = new HashMap();
-                    for (int i = 0; i<line.length; i++){
-                        lineSelected.put(headers[i],line[i]);  //add all row elements into map
+
+            if (args[1].equals("JSON")) {
+                System.out.println(getJsonArray(csvFileName));
+            }
+            else if(args[1].equals("XML")) {
+                String XMLresult = "<"+ csvJustName+">" + XML.toString(getJsonArray(csvFileName)) + "</"+csvJustName+">";  //added root for XML
+                System.out.println(XMLresult);
+            }
+            else {
+                try {
+                    reader = new CSVReader(new FileReader(csvFileName));  //CSVReader manages default comma separated values behaviours.
+                    String[] headers = reader.readNext(); //headers
+                    String[] line;
+
+                    while ((line = reader.readNext()) != null) {
+                        String result = "INSERT INTO " + csvJustName + " (";
+                        for (int i = 0; i < headers.length; i++){
+                            result += headers[i] + ",";
+                        }
+                        result = result.substring(0, result.length() - 1) + ") VALUES ("; //delete last comma
+
+                        for (int i = 0; i < line.length; i++) {
+                            result +=  line[i] + ",";  //add all values
+                        }
+                        result = result.substring(0, result.length() - 1) + ");"; //delete last comma
+                        System.out.println(result);
                     }
-                    itemData.put(lineSelected);  //add map to json result
                 }
-                if (args[1].equals("JSON")) {
-                    System.out.println(itemData);
-                }
-                else {
-                    String XMLresult = "<result>" + XML.toString(itemData) + "</result>";  //added root for XML
-                    System.out.println(XMLresult);
+                catch(IOException e){
+                    e.printStackTrace();
                 }
             }
-            catch(IOException e){
-                e.printStackTrace();
-            }
-
-
         }
 
+    }
+
+    public static JSONArray getJsonArray(String csvFileName){
+        String contents = "";
+        try {
+            contents = Files.lines(Paths.get(csvFileName)).collect(Collectors.joining("\n"));
+        }
+        catch(IOException e){
+            e.printStackTrace();
+        }
+        return CDL.toJSONArray(contents);
     }
 
 
